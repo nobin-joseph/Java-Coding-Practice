4d2af66e3edb15bd1defb7a117d32d2758579eeb
nobin <nobin.joseph51@gmail.com>
2025-10-04 22:15:33 +0530

    edit

--- src/main/java/Beginner/Main.java
+++ src/main/java/Beginner/Main.java
@@ -1,12 +1,34 @@
 package Beginner;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-    DuplicateCharactersCount obj = new DuplicateCharactersCount();
-    obj.counter();
+        String input = "nobin joseph";
+        String Res="";
+
+        String [] s1= input.split(" ");
+        System.out.println(Arrays.toString(s1));
+
+        for(int i=0;i<s1.length;i++ ){
+
+            String s2 = s1[i];
+
+            StringBuilder s3 = new StringBuilder(s2);
+             Res = Res +s3.reverse()+" ";
+        }
+
+            System.out.println(Res);
+        System.out.println(Res);
+        System.out.println(Res);
+        System.out.println(Res);
+
+
+
+
 
     }
 }
---
+++ src/main/java/Beginner/ReverseString.java
@@ -0,0 +1,21 @@
+package Beginner;
+
+public class ReverseString {
+
+    public void reverse (){
+        String input = "nobin joseph";
+        String result ="";
+        char [] charArray = input.toCharArray();
+
+        for(int i=charArray.length-1; i>=0;i--){
+
+            result=result+charArray[i];
+        }
+        System.out.println(result);
+
+        // using string Builder
+        StringBuilder str = new StringBuilder(input);
+        str.reverse();
+
+    }
+}
