67c3816998477797aabbe42277cb90d7fd2a49dc
nobin <nobin.joseph51@gmail.com>
2025-10-16 17:44:51 +0530

    Collection

--- src/main/java/Beginner/Main.java
+++ src/main/java/Beginner/Main.java
@@ -1,25 +1,35 @@
 package Beginner;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 class Main {
 
 
     public static void main(String[] args) {
 
+        String input = "My name is nobin joseph";
 
-        A obj = new B();
+        List<String> word = new ArrayList<>();
+
+        word.addAll(Arrays.asList(input.split(" ")));
+
+        System.out.println(word.reversed());
+
+        ArrayList<String> wordsList =new ArrayList<>();
+
+        wordsList.addAll(Arrays.asList(input.split(" ")));
+
+        Collections.sort(wordsList,Collections.reverseOrder());
+
+        System.out.println();
+
+
+
     }
 }
 
-class A {
-    public void show() {
-        System.out.println("this id in class A ");
-    }
-}
 
-class B extends A{
-
-    public void show(){
-        System.out.println("this id in class B ");
-    };
-
-}
